b2e2cd612440b80d7032fb4686ff69cf40a4690f
mouthdrawing <mouthdrawing@gmail.com>
2019-06-05 23:01:50 +0900

    handling routes with express

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening(){
 	console.log(`listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req,res){
+	res.send("hello from home");
+}
+
+function handleProfile(req,res){
+	res.send("you are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
