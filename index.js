1cd54317109635bb6d0fb8f2a71c1a1cf8c648fe
mouthdrawing <mouthdrawing@gmail.com>
2019-06-06 20:11:25 +0900

    express core: middlewares part two

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -12,12 +16,11 @@ const handleHome = (req, res) =>
 const handleProfile = (req, res) =>
 	res.send("you are on my profile");
 
-const betweenHome = (req, res, next) => {
-	console.log("between");
-	next();
-}
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
