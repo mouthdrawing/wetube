fd29452da1db3e3b527492f3b87470640447a3c8
mouthdrawing <mouthdrawing@gmail.com>
2019-06-06 19:51:04 +0900

    express core: middlewares

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const handleHome = (req, res) =>
 const handleProfile = (req, res) =>
 	res.send("you are on my profile");
 
+const betweenHome = (req, res, next) => {
+	console.log("between");
+	next();
+}
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
