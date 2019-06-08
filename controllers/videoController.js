2431177aaaf4b4dedfa9a05a6672e78adedd67be
mouthdrawing <mouthdrawing@gmail.com>
2019-06-08 16:22:57 +0900

    template variables in pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,12 @@
-export const home = (req, res) => res.render("Home");
-export const search = (req, res) => res.render("Search");
-export const upload = (req, res) => res.render("Upload");
-export const videoDetail = (req, res) => res.render("Video Detail");
-export const editVideo = (req, res) => res.render("Edit Video");
-export const deleteVideo = (req, res) => res.render("Delete Video");
+export const home = (req, res) =>
+	res.render("Home", { pageTitle: "Home" });
+export const search = (req, res) =>
+	res.render("Search", { pageTitle: "Search" });
+export const upload = (req, res) =>
+	res.render("Upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+	res.render("VideoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+	res.render("EditVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+	res.render("DeleteVideo", { pageTitle: "Delete Video" });
