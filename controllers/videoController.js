296c6802c80823febc393b15f395b64c4704c02a
mouthdrawing <mouthdrawing@gmail.com>
2019-06-08 16:47:01 +0900

    search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,21 @@
 export const home = (req, res) =>
 	res.render("Home", { pageTitle: "Home" });
-export const search = (req, res) =>
-	res.render("Search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+	const {
+		query: { term: searchingBy }
+	} = req;
+	res.render("Search", { pageTitle: "Search", searchingBy });
+};
+
 export const upload = (req, res) =>
 	res.render("Upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
 	res.render("VideoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
 	res.render("EditVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
 	res.render("DeleteVideo", { pageTitle: "Delete Video" });
